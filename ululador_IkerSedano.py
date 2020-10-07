5fcd3a0be39afb15ff41e5404f7e9db57ec0fbae
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2020-10-07 09:40:45 +0200

    Add files via upload

---
+++ ululador_IkerSedano.py
@@ -0,0 +1,12 @@
+'''Haz un programa que lea una cadena y sustituya
+las vocales por la letra u. palabra de 10, For'''
+def ululador():
+    palabra=raw_input("Introduce una palabra para ulular")
+    for cont in range(0,100):
+        if (palabra[cont]=='a' or palabra[cont]=='e' or palabra[cont]=='i'or palabra[cont]=='o'):
+            print 'u'
+        else:
+            print palabra[cont]
+    
+                      
+ululador()
