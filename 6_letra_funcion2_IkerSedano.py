52aa5bf50567df4d22bf4ade157c2e48a8539cb8
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2020-10-26 13:47:04 +0100

    Add files via upload

---
+++ 6_letra_funcion2_IkerSedano.py
@@ -0,0 +1,26 @@
+def letra_funcion():
+    print """
+    S de suma
+    R de resta
+    M de multiplicacion
+    D de division"""
+    numero=input ("Introcuzca un numero: ")
+    numero2=input ("Introduzca un numero: ")
+    funcion=raw_input ("Introduzca una letra para definir la operacion: ")
+    if funcion=="S":
+        operacion=numero+numero2
+        print numero,"+",numero2,"=",operacion
+    if funcion=="R":
+        operacion=numero-numero2
+        print numero,"-",numero2,"=",operacion
+    if funcion=="M":
+        operacion=numero*numero2
+        print numero,"*",numero2,"=",operacion
+    if funcion=="D" and numero2==0:
+     
+            print "No computa, brrr, error en el segundo numero, brrr"
+            funcion=raw_input ("Introduzca una letra para definir la operacion: ")          
+        operacion=numero/numero2
+        print numero,"/",numero2,"=",operacion
+    
+letra_funcion()
