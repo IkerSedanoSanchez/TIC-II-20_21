1739ae9a0ee8c029864a81ae32b984d8de670cbe
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2020-11-03 20:41:47 +0100

    Add files via upload

---
+++ 10_numeros.py
@@ -0,0 +1,6 @@
+def numeros():
+    numero=input("Inroduzca un numero entero: ")
+    print "Los numeros anteriores a su numero son: ",(numero-1),(numero-2),(numero-3)
+    print "Los numeros posteriores a su numero son: ",(numero+1),(numero+2),(numero+3)
+numeros()    
+    
