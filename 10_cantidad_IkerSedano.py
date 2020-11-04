a2308583656632eb8c4e96ee19acd429a3b04e88
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2020-11-04 09:45:47 +0100

    Add files via upload

---
+++ 10_cantidad_IkerSedano.py
@@ -0,0 +1,6 @@
+def cantidad():
+    numero=input("Introduzca un numero: ")
+    palabra=raw_input("Introduzca una palabra: ")
+    if [palabra]==numero:
+        print("El numero de letras es el mismo numero que has dicho, muy bien!")
+cantidad()            
