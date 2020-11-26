94619fd73266bfe65432b7adc13601aee649913d
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2020-11-26 12:54:54 +0100

    Add files via upload

---
+++ Numeros 2.cpp
@@ -0,0 +1,16 @@
+//Lee 10 numeros y di el mayor
+#include<stdio.h>
+int main(){
+	int numero[10];
+	int mayor;
+	int cont;
+	for(cont=0;cont<10;cont++){
+		printf("\nNumero %d: ",cont);
+		scanf("%d",&numero[cont]);
+	}
+	printf("La lista");
+	for(cont=0;cont<10;cont++){
+		printf("\nNumero %d: %d",cont,numero[cont]);
+	}
+	return 0;
+}
