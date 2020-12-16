119609821f0e13295dfcf3043f4651fb6a4f99e9
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2020-12-16 09:44:08 +0100

    Add files via upload

---
+++ Arbol.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main;
+	int fila();{
+	printf("Introduce un numero de altura: ");
+	scanf("%d",&arbol);
+	return 0;
+}
+int arbol(int fila){
+	for (fila=1,fila<=altura;fila++){
+		for(col=1;col<=fila,col++){
+			printf("*");
+		}
+	printf("\n")	
+	}
+}
+
+	
+	
