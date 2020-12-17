d1ba17aaa4620193a9dd922665b2e5b8c1431df9
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2020-12-17 12:52:49 +0100

    Add files via upload

--- Arbol.cpp
+++ Arbol.cpp
@@ -1,10 +1,29 @@
 #include<stdio.h>
-int main;
-	int fila();{
-	printf("Introduce un numero de altura: ");
-	scanf("%d",&arbol);
-	return 0;
-}
+/*Introduzca la altura del arbol: 5
+    * 
+   ***
+  *****
+ *******
+*********
+    *
+    *
+Feliz Navidad
+1.Introducir la altura:
+*
+**
+***
+****
+*****
+2.Intentamos la copa del arbol:
+para un arbolde 5
+for(fila=altura-1; fila>=0;fila--)
+fila=1 4 espacios + 1 asterisco
+fila=2 3 espacios + 3 asteriscos
+fila=3 2 espacios + 5 asteriscos
+fila=4 1 espacio + 7 asteriscos
+fila=5 0 espacios + 9 asteriscos
+*/
+int col;
 int arbol(int fila){
 	for (fila=1,fila<=altura;fila++){
 		for(col=1;col<=fila,col++){
@@ -13,6 +32,11 @@ int arbol(int fila){
 	printf("\n")	
 	}
 }
-
+int main;{
+	int fila();
+	printf("Introduce la altura del arbol: ");
+	scanf("%d",&arbol);
+	return 0;
+}
 	
 	
